16ef024254b9ffcb1cfa67662322f390bdfa0158
Sergei Fedotov <fsa@tavda.info>
2023-01-02 23:09:48 +0700

    Перенос index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once "../vendor/autoload.php";
+require_once "vendor/autoload.php";
 
-$file='../archive/club2u.ru/index.php/board,2.0.html';
+$file='archive/club2u.ru/index.php/board,2.0.html';
 
 $board = new \FSA\SMF\Board;
 $board->loadFromFile($file);
