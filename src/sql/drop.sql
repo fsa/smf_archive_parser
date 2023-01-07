984d527d75300050e77b7de42c129d4c8f6f0a8d
Sergei Fedotov <fsa@tavda.info>
2023-01-08 00:01:13 +0700

    Удаление всех таблиц из базы

--- src/sql/drop.sql
+++ src/sql/drop.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS message_icons;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS topics;
 DROP TABLE IF EXISTS boards;
 DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS message_icons;
