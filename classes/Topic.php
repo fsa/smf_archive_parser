07d1d11c565df66b161733883f67317b95f7c706
Sergei Fedotov <fsa@tavda.info>
2023-01-09 04:36:44 +0700

    Первый рабочий вариант конвертора

--- classes/Topic.php
+++ classes/Topic.php
@@ -17,15 +17,23 @@ class Topic
     public function getTopicInfo()
     {
         $a_name = $this->dom->find('#linktree_upper')->find('.last')->find('a');
+        preg_match_all('/board[,=](\d+)\.0/', $this->dom->find('#linktree_upper')->innerHtml, $board_match);
+        $board_id = intval($board_match[1][count($board_match[1]) - 1]);
         return [
             'id' => intval($this->dom->find('input[name="topic"]')->getAttribute('value')),
+            'board_id' => $board_id,
             'title' => $a_name->find('span')->innerHtml
         ];
     }
 
     public function getTopicMessages()
     {
-        $posts = $this->dom->find('#forumposts')->find('form');
+        $posts_forum = $this->dom->find('#forumposts');
+        if (count($posts_forum) == 0) {
+            return [];
+            die('Другой формат');
+        }
+        $posts = $posts_forum->find('form');
         $data = [];
         foreach ($posts->getChildren() as $msg) {
             if (count($msg->find('div.poster')) == 0) {
@@ -41,22 +49,34 @@ class Topic
 
     private function getTopicData($post_dom)
     {
-        $post_inner = $post_dom->find('.post')->find('.inner');
+        $post_post = $post_dom->find('.post');
+        $post_inner = $post_post->find('.inner');
         $post = preg_replace('/\<div class\="quoteheader"\>\<div class\="topslice_quote"\>\<a href\="http\:\/\/www\.club2u\.ru\/index\.php\/topic,(\d+)\.msg(\d+)\.html\#msg(\d+)"\>Цитата: (.*)\<\/a\>\<\/div\>\<\/div\>\<blockquote class\="bbc_standard_quote"\>(.*)\<\/blockquote\>\<div class\="quotefooter"\>\<div class\="botslice_quote"\>\<\/div\>\<\/div>/', '<blockquote msg_id="$2" topic_id="$1" quote="$4">$5</blockquote>', $post_inner->innerHtml);
         $post = preg_replace('/\<div class\="quoteheader"\>\<div class\="topslice_quote"\>Цитата: (.*)\<\/div\>\<\/div\>\<blockquote class\="bbc_standard_quote"\>(.*)\<\/blockquote\>\<div class\="quotefooter"\>\<div class\="botslice_quote"\>\<\/div\>\<\/div>/', '<blockquote quote="$1">$2</blockquote>', $post);
-
+        // Отличается от первого URL
+        $post = preg_replace('/\<div class\="quoteheader"\>\<div class\="topslice_quote"\>\<a href\="http\:\/\/www\.club2u\.ru\/index\.php\?topic\=(\d+)\.msg(\d+)#msg(\d+)"\>Цитата: (.*)\<\/a\>\<\/div\>\<\/div\>\<blockquote class\="bbc_standard_quote"\>(.*)\<\/blockquote\>\<div class\="quotefooter"\>\<div class\="botslice_quote"\>\<\/div\>\<\/div>/', '<blockquote msg_id="$2" topic_id="$1" quote="$4">$5</blockquote>', $post);
         $keyinfo_dom = $post_dom->find('.postarea')->find('.keyinfo');
         $icon = basename($keyinfo_dom->find('img')->getAttribute('src'));
         preg_match('/&\#171; \<strong\>(.*) \:\<\/strong\> (.*) &\#187;/', $keyinfo_dom->find('.smalltext')->innerHtml, $post_info);
         $posted = Tools::getDatetimeFromText($post_info[2]);
         $subject = $keyinfo_dom->find('h5')->find('a')->innerHtml;
 
-        $poster_a = $post_dom->find('div')->find('.poster')->find('h4')->find('a');
+        $poster_h4 = $post_dom->find('div')->find('.poster')->find('h4');
+        $poster_a = $poster_h4->find('a');
+        if (count($poster_a) > 0) {
+            $li = $poster_a->find('li');
+            $poster = count($li) ? $li[0]->innerHtml : $poster_a->innerHtml;
+            $member_id = Tools::getUserIdFromUrl($poster_a->getAttribute('href'));
+        } else {
+            $li = $poster_h4->find('li');
+            $poster = count($li) ? $li[0]->innerHtml : $poster_h4->innerHtml;
+            $member_id = null;
+        }
         $result = [
             'id' => intval(trim($post_inner->id, 'msg_')),
             'post' => $post,
-            'poster_name' => $poster_a->innerHtml,
-            'member_id' => Tools::getUserIdFromUrl($poster_a->getAttribute('href')),
+            'poster_name' => $poster,
+            'member_id' => $member_id,
             'icon' => trim($icon, '.gif'),
             'posted' => $posted,
             'subject' => $subject
--- convertor.php
+++ convertor.php
@@ -5,7 +5,7 @@ use FSA\SMF\Tools;
 
 require_once "vendor/autoload.php";
 $settings = require 'settings.php';
-$tz = $settings['TIMEZONE']??'Asia/Novosibirsk';
+$tz = $settings['TIMEZONE'] ?? 'Asia/Novosibirsk';
 $path = "archive/{$settings['SITE_URL']}/";
 
 date_default_timezone_set($tz);
@@ -62,6 +62,58 @@ foreach ($files as $item) {
     //var_dump($topics);
     foreach ($topics as $topic) {
         $stmt->execute([$topic->id, $board_id, $topic->title]);
-        echo "  Добавлен топик №{$topic->id}: {$topic->title}" . PHP_EOL;
+        if ($stmt->rowCount() > 0) {
+            echo "  Добавлен топик №{$topic->id}: {$topic->title}" . PHP_EOL;
+        } else {
+            echo "  Дубликат топика №{$topic->id}: {$topic->title}" . PHP_EOL;
+        }
+    }
+}
+
+// Messages
+$match = '/^topic,(.*)\.(.*).html$/';
+$files = preg_grep($match, $all_files);
+foreach ($files as $item) {
+    $file = $path . 'index.php/' . trim($item);
+
+    # Получение id из имени файла
+    preg_match($match, $item, $search);
+    $topic_id = intval($search[1]);
+
+    echo 'Обработка файла ' . $file . PHP_EOL;
+    $topic = new \FSA\SMF\Topic;
+    $topic->loadFromFile($file);
+
+    $stmt = $pdo->prepare('SELECT id FROM topics WHERE id=?');
+    $stmt->execute([$topic_id]);
+    if (!$stmt->fetch(PDO::FETCH_COLUMN)) {
+        $topic_info = $topic->getTopicInfo();
+        $stmt = $pdo->prepare('INSERT INTO topics (id, board_id, title) VALUES (:id, :board_id, :title)');
+        $stmt->execute($topic_info);
+        echo "  Добавлен топик №{$topic_info['id']}: {$topic_info['title']}" . PHP_EOL;
+    }
+
+    $messages = $topic->getTopicMessages();
+    // Users
+    $stmt = $pdo->prepare('INSERT INTO members (id, name) VALUES (?,?) ON CONFLICT (id) DO NOTHING');
+    foreach ($messages as $message) {
+        if ($message->member_id) {
+            $stmt->execute([$message->member_id, $message->poster_name]);
+            if ($stmt->rowCount() > 0) {
+                echo "  Добавлен пользователь №{$message->member_id}: {$message->poster_name}" . PHP_EOL;
+            }
+        }
+    }
+    $stmt = $pdo->prepare('INSERT INTO messages (id, topic_id, posted, member_id, subject, poster_name, body, icon) VALUES (:id, :topic_id, :posted, :member_id, :subject, :poster_name, :post, :icon) ON CONFLICT (id) DO NOTHING');
+    foreach ($messages as $message) {
+        $obj = get_object_vars($message);
+        $obj['topic_id'] = $topic_id;
+        $stmt->execute($obj);
+        if ($stmt->rowCount() > 0) {
+            echo "  Добавлено сообщение от {$message->posted} №{$message->id}, пользователь {$message->poster_name}" . PHP_EOL;
+        } else {
+            echo "  Дубликат сообщения от {$message->posted} №{$message->id}, пользователь {$message->poster_name}" . PHP_EOL;
+
+        }
     }
 }
