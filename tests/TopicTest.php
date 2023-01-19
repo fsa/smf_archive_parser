83e5794158cea418d28a8af9f5903df12b10773e
Sergei Fedotov <fsa@tavda.info>
2023-01-20 02:38:54 +0700

    Чистка кода и перенос тестов в PHPUnit

--- tests/TopicTest.php
+++ tests/TopicTest.php
@@ -16,24 +16,38 @@ final class TopicTest extends TestCase
 
         $this->topic311 = new Topic;
         $this->topic311->loadFromFile(__DIR__ . '/archive/index.php?topic=311.msg19476');
+
+        // archive/club2u.ru/index.php/topic,467.0.html
     }
 
     public function testGetTopicInfo()
     {
         $result266 = $this->topic266->getTopicInfo();
         $this->assertEquals(
-            ['id' => 266, 'board_id' => 8, 'title' => "Бета-версия нового Ю-кабинета."],
+            [
+                'id' => 266,
+                'board_id' => 8,
+                'title' => "Бета-версия нового Ю-кабинета."
+            ],
             $result266
         );
 
         $result311 = $this->topic311->getTopicInfo();
-        $this->assertIsArray($result311);
+        $this->assertEquals(
+            [
+                'id' => 311,
+                'board_id' => 14,
+                'title' => "Настройка торрентов"
+            ],
+            $result311
+        );
     }
 
     public function testGetTopicMessages()
     {
         $result266 = $this->topic266->getTopicMessages();
         $this->assertIsArray($result266);
+        $this->assertGreaterThan(0, count($result266));
 
         $result311 = $this->topic311->getTopicMessages();
         $this->assertIsArray($result311);
