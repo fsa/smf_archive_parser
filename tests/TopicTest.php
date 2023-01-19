c30ca4f5cc95f653a269f2063a6edd2473d2e38a
Sergei Fedotov <fsa@tavda.info>
2023-01-20 02:24:47 +0700

    Доработка тестов и тестовые файлы

--- tests/TopicTest.php
+++ tests/TopicTest.php
@@ -6,25 +6,37 @@ use PHPUnit\Framework\TestCase;
 final class TopicTest extends TestCase
 {
 
-    private $topic;
+    private $topic266;
+    private $topic311;
 
     protected function setUp(): void
     {
-        $this->topic = new Topic;
-        $this->topic->loadFromFile(__DIR__.'/../archive/club2u.ru/index.php?topic=266.0');
+        $this->topic266 = new Topic;
+        $this->topic266->loadFromFile(__DIR__ . '/archive/index.php?topic=266.0');
+
+        $this->topic311 = new Topic;
+        $this->topic311->loadFromFile(__DIR__ . '/archive/index.php?topic=311.msg19476');
     }
 
     public function testGetTopicInfo()
     {
+        $result266 = $this->topic266->getTopicInfo();
         $this->assertEquals(
-            ['id' =>266, 'board_id' =>8, 'title' => "Бета-версия нового Ю-кабинета."],
-            $this->topic->getTopicInfo()
+            ['id' => 266, 'board_id' => 8, 'title' => "Бета-версия нового Ю-кабинета."],
+            $result266
         );
+
+        $result311 = $this->topic311->getTopicInfo();
+        $this->assertIsArray($result311);
     }
 
     public function testGetTopicMessages()
     {
-        $result = $this->topic->getTopicMessages();
-        $this->assertIsArray($result);
+        $result266 = $this->topic266->getTopicMessages();
+        $this->assertIsArray($result266);
+
+        $result311 = $this->topic311->getTopicMessages();
+        $this->assertIsArray($result311);
+        $this->assertGreaterThan(0, count($result311));
     }
 }
